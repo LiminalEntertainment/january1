1cc32c02f2393ff3633d1bd6217406283a899c41
LiminalEntertainment <ve4n1x@gmail.com>
2026-01-13 12:16:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.title("favourite things")
+st.title("Favourite things")
 
-# Инициализация на данните
+# Initialize data
 if "subjects" not in st.session_state:
     st.session_state.subjects = {
         "Math": 0,
@@ -16,17 +16,17 @@ if "sports" not in st.session_state:
     st.session_state.sports = {
         "Football": 0,
         "Basketball": 0,
-        "Volleybal": 0,
+        "Volleyball": 0,
         "Swimming": 0
     }
 
-st.subheader("Select your favourite from the following: ")
+st.subheader("Select your favourite from the following:")
 
-subjects = st.selectbox("Favourite subject:", list(st.session_state.subjects.keys()))
-sport = st.selectbox("Favourite sports:", list(st.session_state.sports.keys()))
+subject = st.selectbox("Favourite subject:", list(st.session_state.subjects.keys()))
+sport = st.selectbox("Favourite sport:", list(st.session_state.sports.keys()))
 
 if st.button("Save"):
-    st.session_state.subjects[subjects] += 1
+    st.session_state.subjects[subject] += 1
     st.session_state.sports[sport] += 1
     st.success("Saved")
 
@@ -34,17 +34,16 @@ st.divider()
 
 st.subheader("Results")
 
-# Графика за цветовете
+# Subjects chart
 st.write("Favourite subjects")
-colors_df = pd.DataFrame.from_dict(
-    st.session_state.subjects, orient="index", columns=["Брой"]
+subjects_df = pd.DataFrame.from_dict(
+    st.session_state.subjects, orient="index", columns=["Count"]
 )
 st.bar_chart(subjects_df)
 
-# Графика за спортовете
+# Sports chart
 st.write("Favourite sports")
 sports_df = pd.DataFrame.from_dict(
-    st.session_state.sports, orient="index", columns=["Брой"]
+    st.session_state.sports, orient="index", columns=["Count"]
 )
 st.bar_chart(sports_df)
-
